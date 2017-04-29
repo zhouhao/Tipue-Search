fed45f5eced61354f6094df4379b2bee2537272d
Hao Zhou <hzhou@tripadvisor.com>
2017-04-29 16:43:52 -0400

    keep json as the only mode

--- tipuesearch/tipuesearch.js
+++ tipuesearch/tipuesearch.js
@@ -22,7 +22,6 @@
             'liveContent': '*',
             'liveDescription': '*',
             'minimumLength': 3,
-            'mode': 'static',
             'newWindow': false,
             'show': 9,
             'showContext': true,
@@ -46,43 +45,9 @@
 
             $('#tipue_search_content').hide().html('<div class="tipue_search_spinner"><div class="tipue_search_rect1"></div><div class="tipue_search_rect2"></div><div class="rect3"></div></div>').show();
 
-            if (set.mode == 'live') {
-                for (var i = 0; i < tipuesearch_pages.length; i++) {
-                    $.get(tipuesearch_pages[i]).done(function (html) {
-                        var cont = $(set.liveContent, html).text();
-                        cont = cont.replace(/\s+/g, ' ');
-                        var desc = $(set.liveDescription, html).text();
-                        desc = desc.replace(/\s+/g, ' ');
-
-                        var t_1 = html.toLowerCase().indexOf('<title>');
-                        var t_2 = html.toLowerCase().indexOf('</title>', t_1 + 7);
-                        if (t_1 != -1 && t_2 != -1) {
-                            var tit = html.slice(t_1 + 7, t_2);
-                        }
-                        else {
-                            var tit = tipuesearch_string_1;
-                        }
-
-                        tipuesearch_in.pages.push(
-                            {
-                                "title": tit,
-                                "text": desc,
-                                "tags": cont,
-                                "url": tipuesearch_pages[i]
-                            });
-                    });
-                }
-            }
-
-            if (set.mode == 'json') {
-                $.getJSON(set.contentLocation).done(function (json) {
-                    tipuesearch_in = $.extend({}, json);
-                });
-            }
-
-            if (set.mode == 'static') {
-                tipuesearch_in = $.extend({}, tipuesearch);
-            }
+            $.getJSON(set.contentLocation).done(function (json) {
+                tipuesearch_in = $.extend({}, json);
+            });
 
             var tipue_search_w = '';
             if (set.newWindow) {
